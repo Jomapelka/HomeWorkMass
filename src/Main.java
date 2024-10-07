3d5f53323988746ab49772682a201456e923f94a
Jomapelka <Belyashnoe.narezalovo@gmail.com>
2024-10-07 12:44:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] heights = new int[12];
+        Random random = new Random();
+
+        for (int i = 0; i < heights.length; i++) {
+            heights[i] = random.nextInt(28) + 163;
+            System.out.println(heights[i]);
+        }
     }
 }
